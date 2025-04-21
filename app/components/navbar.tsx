68b3b128729dc609cdbddd5a84935603e50ad834
”ns-develops” <test.tt9061030@gmail.com>
2025-04-21 14:44:28 +0200

    navbar before css

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
 
-
 export const Navbar = () => {
     return (
-    <nav>
-        <div>   
-        <Link href="/">Your E-commerce Company</Link>
-        </div>    
-    </nav>
+        <nav className="sticky top-0 z-50 bg-white shadow-sm w-full -webkit-sticky">
+            <div className="container mx-auto flex items-center justify-between px-4 py-4">
+                <Link href="/" className="hover:text-blue-600 text-xl font-semibold text-black">
+                    Your E-commerce
+                </Link>
+
+                <div className="flex space-x-6 ml-auto">
+                    <Link href="/new-in" className="hover:text-blue-600 text-black">
+                        New in
+                    </Link>
+                    <Link href="/clothes" className="hover:text-blue-600 text-black">
+                        Clothes
+                    </Link>
+                    <Link href="/checkout" className="hover:text-blue-600 text-black">
+                        Shopping cart
+                    </Link>
+                </div>
+            </div>
+        </nav>
     );
-}
+};
