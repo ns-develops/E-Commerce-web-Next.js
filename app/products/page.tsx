73e177a0170c6517a232a1e1a2de4aecf5c07efe
”ns-develops” <test.tt9061030@gmail.com>
2025-05-01 13:09:19 +0200

    removing module.css for deployment

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,10 @@
 import { stripe } from "@/lib/stripe";
-import styles from "./page.module.css";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from 'next/link';
 import Carousel from "@/components/carousel";
 
-
 export default async function ProductsPage() {
-   
     const result = await stripe.products.list({
         expand: ["data.default_price"],
         limit: 5,
@@ -15,35 +12,40 @@ export default async function ProductsPage() {
 
     const products = result.data;
 
-    console.log(products); 
+    console.log(products);
 
     return (
         <div>
-            <section className="rounded bg-neautral-100 py-8 sm:py-12">
-                <div className="mx-auto grid grid grid-cols-1 items-center justify-items-center gap-8 sm:px-16">
+            <section className="bg-neutral-100 py-8 sm:py-12">
+                <div className="mx-auto flex items-center justify-center gap-8 sm:px-16">
                     <div className="max-w-md space-y-4">
-                        <h2 className="text 3xl font-bold tracking-tight md:text 4xl">
+                        <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
                             Welcome to My Ecommerce
-                            </h2>
+                        </h2>
                         <p className="text-neutral-600">
                             See what’s new — thoughtfully made for people and planet
-                            </p>
-                        <Button asChild variant="default" className="inline-flex items-center justify-center rounded-full px-6 py3 bg-black text-white">
-                            <Link href="/products" className="inline-flex items-center justify-center rounded-full px-6 py3">Browse all products</Link>
+                        </p>
+                        <Button asChild variant="default" className="">
+                            <Link href="/products" className="inline-flex items-center justify-center rounded-full px-6 py-3">
+                                Browse all products
+                            </Link>
                         </Button>
                     </div>
                     {products.length > 0 && products[0].images.length > 0 && (
-                        <Image 
-                            alt="Banner Image" 
-                            width={450} 
-                            height={450} 
-                            src={products[0].images[0]} 
-                        />
+                        <div className="flex-shrink-0">
+                            <Image
+                                alt="Banner Image"
+                                width={450}
+                                height={450}
+                                src={products[0].images[0]}
+                                className="rounded-lg"
+                            />
+                        </div>
                     )}
                 </div>
             </section>
             <section>
-                <Carousel/>
+                <Carousel />
             </section>
         </div>
     );
