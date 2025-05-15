839284557a41f5910f74a3aa4fef4d9746eb1415
”ns-develops” <test.tt9061030@gmail.com>
2025-05-15 06:29:32 +0200

    upd letters animate new-in

--- app/new-in/page.tsx
+++ app/new-in/page.tsx
@@ -1,3 +1,15 @@
+import React from "react";
+import "./page2.css"; 
+
 export default function NewInPage() {
-    return <div>New in</div>
+    return (
+        <div className="new-in-container">
+            <div className="new-in-text">
+                New-In Clothes
+            </div>
+            <div className="sustainable-text">
+                Sustainable
+            </div>
+        </div>
+    );
 }
