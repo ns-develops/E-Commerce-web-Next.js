2b944f18d01544104afd32d309a5cfbed79ac188
”ns-develops” <test.tt9061030@gmail.com>
2025-05-15 04:47:57 +0200

    change layout searchfield with icon

--- components/ui/product-list.tsx
+++ components/ui/product-list.tsx
@@ -1,12 +1,11 @@
-"use client"; 
+"use client";
 
-
+import Icon from '@mdi/react';
+import { mdiMagnify } from '@mdi/js';
 import Stripe from "stripe";
 import { useState } from "react";
 import { ProductCard } from "./product-card";
 import "@/components/ui/product-list.css"; 
- 
-
 
 interface Props {
   products: Stripe.Product[];
@@ -14,30 +13,46 @@ interface Props {
 
 export const ProductList = ({ products }: Props) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
- 
+  const toggleSearch = () => {
+    setIsSearchVisible(!isSearchVisible);
+  };
+
   const filteredProducts = products.filter((product) =>
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
     <div className="space-y-6">
-  
-      <div className="flex justify-center mb-6">
-        <input
-          type="text"
-          placeholder="Search products..."
-          className="search-input"
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />
+      <div className="flex justify-between items-center mb-6">
+        <div className="search-container">
+         
+          {isSearchVisible && (
+            <input
+              type="text"
+              placeholder=""
+              className="search-input"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              autoFocus
+            />
+          )}
+
+          <div onClick={toggleSearch} className="search-icon">
+            <Icon
+              path={mdiMagnify}
+              size={1}
+              className="search-icon"
+            />
+          </div>
+        </div>
       </div>
 
-  
       <div className="product-grid">
         {filteredProducts.map((product) => (
           <div key={product.id} className="product-card-container">
