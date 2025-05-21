45aadc814508e19fba692bbf3da151164090d333
nasa01-crypto <natalie.samaan@live.se>
2025-05-21 23:58:37 +0200

    deploy2

--- app/new-in/page.tsx
+++ app/new-in/page.tsx
@@ -1,28 +1,26 @@
-import React from "react";
-import "./page2.css"; 
-import NewInCarousel from "./NewInCarousel"; 
+// app/new-in/page.tsx
+
+import Image from "next/image";
+import NewInCarousel from "./NewInCarousel";
+import "./page2.css";
 
 export default function NewInPage() {
   return (
     <div className="new-in-container">
-      <div className="new-in-text">
-        New-In Clothes
-      </div>
-      <div className="sustainable-text">
-        Sustainable
-      </div>
-
+      <div className="new-in-text">New-In Clothes</div>
+      <div className="sustainable-text">Sustainable</div>
 
       <div className="carousel-container">
         <NewInCarousel>
           <div className="carousel-slide">
-            <img src="/image2.jpg" alt="Image 2" width={600} height={400} />
+            <Image src="/image2.jpg" alt="Image 2" width={600} height={400} />
           </div>
           <div className="carousel-slide">
-            <img src="/image1.jpg" alt="Image 1" width={600} height={400} />
+            <Image src="/image1.jpg" alt="Image 1" width={600} height={400} />
           </div>
         </NewInCarousel>
       </div>
     </div>
   );
 }
+
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,7 @@
 import { stripe } from "@/lib/stripe";
 import { ProductDetail } from "@/components/product-detail";
 
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function ProductPage({ params }: ProductPageProps) {
+export default async function ProductPage({ params }: { params: { id: string } }) {
   const product = await stripe.products.retrieve(params.id, {
     expand: ["default_price"],
   });
