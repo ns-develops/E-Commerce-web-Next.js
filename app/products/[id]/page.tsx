18b4fdcfb1def172303d8f9d5865ea094ad2e1e0
nasa01-crypto <natalie.samaan@live.se>
2025-05-22 08:35:24 +0200

    removing props,causing deoployment issues

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,15 @@
 import { stripe } from "@/lib/stripe";
 import { ProductDetail } from "@/components/product-detail";
+import Stripe from "stripe";
 
-export default async function ProductPage({ params }: { params: { id: string } }) {
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const product = await stripe.products.retrieve(params.id, {
     expand: ["default_price"],
   });
 
-  return <ProductDetail product={product} />;
+  return <ProductDetail product={product as Stripe.Product} />;
 }
