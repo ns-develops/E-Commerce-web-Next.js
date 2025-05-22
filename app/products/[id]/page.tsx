7c69a7bed90b19ec12c0d1d3096376c5fac9e9c1
nasa01-crypto <natalie.samaan@live.se>
2025-05-22 08:39:23 +0200

    new try

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,14 +2,17 @@ import { stripe } from "@/lib/stripe";
 import { ProductDetail } from "@/components/product-detail";
 import Stripe from "stripe";
 
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ProductPage({ params }: Props) {
   const product = await stripe.products.retrieve(params.id, {
     expand: ["default_price"],
   });
 
   return <ProductDetail product={product as Stripe.Product} />;
 }
+
